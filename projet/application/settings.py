e10254411c5ab273fcc40c6892deb1c3e9ee85d5
ometeore <pilt64@hotmail.fr>
2020-01-15 19:25:20 +0100

    deploy heroku 7

--- projet/application/settings.py
+++ projet/application/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import django_heroku
+
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,7 +85,6 @@ WSGI_APPLICATION = "application.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "nuts_lover",}}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
