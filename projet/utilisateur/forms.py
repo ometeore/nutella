dc8249829aafa88410caefaf4e61d0a7b8bba841
ometeore <pilt64@hotmail.fr>
2020-01-22 14:36:55 +0100

    front inscription utilisateur

--- projet/utilisateur/forms.py
+++ projet/utilisateur/forms.py
@@ -7,9 +7,9 @@ class Connexion(forms.Form):
 
 
 class Creation(forms.Form):
-    last_name = forms.CharField(label="Nom", max_length=100, widget=forms.TextInput(attrs={"class":""}))
-    first_name = forms.CharField(label="Prénom", max_length=100)
-    Username = forms.CharField(label="Pseudo", max_length=100)
-    email = forms.EmailField(label="email")
-    confirm_password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput())
-    password = forms.CharField(label="Confirmation mot de passe", widget=forms.PasswordInput())
+    last_name = forms.CharField(label="Nom", max_length=100, widget=forms.TextInput(attrs={'placeholder':"Nom"}))
+    first_name = forms.CharField(label="Prénom", max_length=100, widget=forms.TextInput(attrs={'placeholder':"Prenom"}))
+    Username = forms.CharField(label="Pseudo", max_length=100, widget=forms.TextInput(attrs={'placeholder':"Pseudo"}))
+    email = forms.EmailField(label="email", widget=forms.TextInput(attrs={'placeholder':"email"}))
+    confirm_password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={'placeholder':"Mot de passe"}))
+    password = forms.CharField(label="Confirmation mot de passe", widget=forms.PasswordInput(attrs={'placeholder':"Confirmation mot de passe"}))
