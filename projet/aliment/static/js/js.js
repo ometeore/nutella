126c5803bf35ead07eed5cd13eb4463bd5b410c0
ometeore <pilt64@hotmail.fr>
2020-01-29 19:03:12 +0100

    ajx recherche

--- projet/aliment/static/js/js.js
+++ projet/aliment/static/js/js.js
@@ -5,7 +5,7 @@ function show(){
 
 }
 
-function maFonction(aliment_search){
+function validate_aliment(aliment_search){
 
   $.ajax({
     url:'/aliment/validation',
@@ -16,11 +16,11 @@ function maFonction(aliment_search){
     success: function (data) {
       if(data.is_taken) {
         console.log('ok il y a encore des aliments qui match');
-        document.getElementById("element_search").style.border = "2px solid #00FF00;" 
+        document.getElementById("element_search").style.border = "2px solid #00FF00"; 
       }
       else{
         console.log("Nous n'avons rien trouvé correspondant a la recherche " + aliment_search);
-        document.getElementById("element_search").style.border = "2px solid #FF0000;" 
+        document.getElementById("element_search").style.border = "2px solid #FF0000"; 
       }
     }
   })
