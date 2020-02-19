938fb6c2eada2115b21d65d38fe291541bdec319
ometeore <pilt64@hotmail.fr>
2020-02-19 16:02:20 +0100

    before clonage

--- projet/staticfiles/js/js.js
+++ projet/staticfiles/js/js.js
@@ -2,7 +2,6 @@ function show(){
     document.getElementById("section_substitute").style.display = "block";
     document.getElementById("remove").style.display = "none";
     document.getElementById("remove2").style.display = "none";
-
 }
 
 function validate_aliment(aliment_search){
